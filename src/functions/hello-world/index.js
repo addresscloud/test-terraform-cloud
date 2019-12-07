fff1c6e1f1ac36ec73baeacda302627f2919c793
Tomas Holderness <tomas@addresscloud.com>
2019-12-07 09:23:56 +0000

    force code update

--- src/functions/hello-world/index.js
+++ src/functions/hello-world/index.js
@@ -1,8 +1,8 @@
 // Hello world function handler
-// Force function update
 export default async(e, ctx, cb) => {
     try {
         console.log(`Hello World`)
+        console.log(`Force update`)
     } catch (err) {
         console.error(err.message)
     }
