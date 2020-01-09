73a2ceb1c005b6dff70b36d7261ed6d525c60ca0
Tomas <tomasholderness@gmail.com>
2020-01-09 17:41:36 +0000

    add comment

--- src/functions/hello-world/index.js
+++ src/functions/hello-world/index.js
@@ -3,6 +3,7 @@ console.log(`Initialising...`)
 export default async(e, ctx, cb) => {
     try {
         console.log(`Hello World`)
+        console.log(`Complete`)
     } catch (err) {
         console.error(err.message)
     }
